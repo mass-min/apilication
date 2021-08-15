1684b4d695db021ee41b05aaa747537fb16db7b6
mass-min <koyod111@gmail.com>
2021-08-15 21:16:05 +0800

    refs fix schema name

--- src/pages/api/apply.tsx
+++ src/pages/api/apply.tsx
@@ -1,10 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { personSchema } from '../../schemas/person'
+import { applicationSchema } from '../../schemas/application'
 import validate from '../../lib/validate'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    await validate(personSchema, req)
+    await validate(applicationSchema, req)
     res.status(200).json({ ...req.body, method: req.method })
   } catch (error) {
     res.status(400).json(error)
--- src/schemas/application.tsx
+++ src/schemas/application.tsx
@@ -1,8 +1,8 @@
 import { object, string, array, number, TypeOf } from 'yup'
 
-export const personSchema = object({
+export const applicationSchema = object({
   name: string().required().min(2).max(10),
   email: string().required().email(),
 })
 
-export type Person = TypeOf<typeof personSchema>
+export type Application = TypeOf<typeof applicationSchema>
